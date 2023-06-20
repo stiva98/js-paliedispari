83e401c2644ea49f119bbc3e8053cd575c169b80
stiva98 <samustiva9810@gmail.com>
2023-06-20 23:06:23 +0200

    ADD function

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,15 @@ if (word == reverseWord) {
     console.log('la parola non è palindroma');
 }
 
+function reverse(array) {
+    let reverseWord = '';
+
+    let i = array.length - 1;
+
+    while (i >= 0) {
+        reverseWord += array[i];
+        i--;
+    }
+
+    return reverseWord;
+}
