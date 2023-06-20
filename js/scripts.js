6d67c5667053ccb0bc6d5f704ac75ab40a9bd648
stiva98 <samustiva9810@gmail.com>
2023-06-20 23:14:44 +0200

    ADD stampa in html e finito palindromi

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 const word = prompt();
 let reverseWord = reverse(word);
+const myP = document.getElementById('press');
 
 if (word == reverseWord) {
-    console.log('la parola è palindroma');
+    //console.log('la parola è palindroma');
+    myP.innerHTML= 'La parola è palindroma'
 } else {
-    console.log('la parola non è palindroma');
+    //console.log('la parola non è palindroma');
+    myP.innerHTML= 'La parola NON è palindroma'
 }
 
 function reverse(array) {
