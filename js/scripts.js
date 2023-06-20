736702de2a9ebe7071a3f625898a06b18e9bf829
stiva98 <samustiva9810@gmail.com>
2023-06-20 22:57:09 +0200

    ADD if e else

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 const word = prompt();
 let reverseWord = reverse(word);
 
+if (word == reverseWord) {
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
+
