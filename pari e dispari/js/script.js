6b6d68aadf50880214fa31d98a00015c644f49cf
stiva98 <samustiva9810@gmail.com>
2023-06-21 23:21:14 +0200

    ADD aggiungo fine + terminato esercizio

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -4,27 +4,46 @@ function getRndInteger() {
     //console.log(Math.floor(Math.random() * (6 - 1) ) + 1)
     return Math.floor(Math.random() * (6 - 1) ) + 1;
 }
-function itsEven(sum) {
-    if (sum % 2 == 0) {
+function itsEvenOrOdd(sum) {
+    if (sum % 2 === 0) {
         console.log('è pari')
+        return 'pari'
     }else {
         console.log('è dispari')
+        return 'dispari'
     }
 }
-myButton.addEventListener('click', function(){
-    let sum = 0
-    const evenOrOdd = prompt('Inserisci pari o dispari')
-    console.log(evenOrOdd)
-    const insertNumber = prompt('Inserisci un numero da 1 a 5')
-    console.log(insertNumber)
- 
+myButton.addEventListener('click', evenOrOdd);
+    let sum = 0;
+    function evenOrOdd() {
+        const userChoice = prompt('Inserisci pari o dispari:')
+        console.log(userChoice)
+        if (userChoice.toLowerCase() == 'pari' || userChoice.toLowerCase() == 'dispari') {
+        console.log('Ok sai scrivere')
+        }else{
+            return alert('Errore')
+        }
+        //return itsEvenOrOdd(userChoice);
+    
+
+
+    const userNumber = prompt('Inserisci un numero da 1 a 5')
+    console.log(userNumber)
+    if (userNumber > 1 && userNumber <= 5){
+        console.log('ok')
+    }else {
+        return alert('Errore')
+    }
+    
     const numberRandomPc = getRndInteger();
     console.log(numberRandomPc)
-    debugger
-    sum = numberRandomPc + insertNumber;
+    sum = numberRandomPc + parseInt(userNumber);
     console.log(sum)
     
-    itsEven(sum)
-});
+    if (itsEvenOrOdd(sum) == userChoice){
+        console.log('hai vinto')
+    }else {
+        console.log('hai perso')
+    }
 
-
+}
